1b1822dd869c73b33f3247174ffe6cc5f18fc5c4
Sergey Mezhmal <s.mezhmal@gmail.com>
2025-03-02 22:08:38 +0300

    move assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: './docs'
+  outDir: './docs',
+  build: {
+    assets: 'assets'
+  }
 });
