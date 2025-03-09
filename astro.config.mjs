b1737ecfe9b468538121af22b5e3c709c387ed1b
Sergey Mezhmal <s.mezhmal@gmail.com>
2025-03-09 18:00:02 +0300

    reset outDir to default

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mezhmal.github.io',
-  outDir: './docs',
   build: {
     assets: 'assets'
   }
